85cae2e2a292c7b3847d4ca525766cdc18349346
Jate099 <42478567+Jate099@users.noreply.github.com>
2019-04-04 22:30:33 -0500

    se agerga el readFile donde se pueden ver los numeros de visitas en la pagina Home

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 
+const fs = require('fs');
+
 app.use(express.static('public'));
 
 var contador = 0;
@@ -9,7 +11,21 @@ console.log(__dirname);
 
 app.get('/', function (req, response) {
   response.sendFile(__dirname + '/public/home.html');
+
   contador++;
+
+  function archivoEscrito(){
+    console.log("el archivo se creo");
+}
+
+fs.writeFile('message.txt', contador, 'utf8', archivoEscrito);
+
+//leer archivo
+fs.readFile('message.txt', 'utf8', function(err, data){
+    if(err) throw err;
+    console.log(data);
+});
+
 });
 
 app.get('/contacto', function (request, response) {
